e31652f6fdbe48cd1c1a6458e3153caf17f2488e
manak1 <mana23happy@hotmail.co.jp>
2022-01-24 18:26:32 +0900

    feat: 買い物比較のプログラムを書いた

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,4 +1,18 @@
 fn main() {
+   let pc = 98000.0;
+   let a_rate = 0.8;
+   let b_rate = 0.9;
+   let ship_price = 1200.0;
+
+   let a_price = pc * a_rate + ship_price;
+   let b_price = pc * b_rate;
+
+    println!("A社の値段は{}円です。", a_price);
+    println!("B社の値段は{}円です。", b_price);
+
+}
+
+/* fn fibonacci() {
     let mut a = 1;
     let mut b = 1;
     println!("{}", a);
@@ -9,4 +23,4 @@ fn main() {
         a = b;
         b = tmp+a;
     }
-}
+} */
