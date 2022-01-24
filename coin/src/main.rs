cb4e4ad5ce23986637c58511b6cffc949ea51fa0
manak1 <mana23happy@hotmail.co.jp>
2022-01-24 23:14:50 +0900

    feat: おつりプログラムに型を追加

--- coin/src/main.rs
+++ coin/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let price = 3950;
-    for i500 in 0..11 {
-        for i100 in 0..4 {
-            for i50 in 0..11 {
-                let sum = 500 * i500 + 100 * i100 + 50 * i50;
+    let price: i64 = 3950;
+    let count500: i64 = 10;
+    let count10: i64 = 3;
+    let count50: i64 = 10;
+
+    for i500 in 0..(count500+1) {
+        for i100 in 0..(count10+1) {
+            for i50 in 0..(count50+1) {
+                let sum: i64 = 500 * i500 + 100 * i100 + 50 * i50;
                 if sum == price {
                     println!("500円{}枚玉100円玉{}枚50円玉{}枚で{}円です。", i500, i100, i50, price);
                 }
