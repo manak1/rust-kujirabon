2147ca74c52764b8b42e4561ae3d42682a2c6c2d
manak1 <mana23happy@hotmail.co.jp>
2022-01-25 15:37:33 +0900

    feat: クロージャーを利用する形で暗号化プログラムを書き換える

--- caesar/src/main.rs
+++ caesar/src/main.rs
@@ -2,25 +2,22 @@ fn main() {
     let enc = encrypt("HELLO RUST",3);
     let dec = encrypt(&enc, -3);
     println!("{} => {}", enc, dec);
+
+    let double     = |n: i64| n * 2;
+    println!("{}",double(2));
+
+
 }
 
 fn encrypt(text: &str, shift: i16) -> String {
     let code_a = 'A' as i16;
-    let code_z = 'Z' as i16;
-
     let mut result = String::new();
 
-    for ch in text.chars() {
-        let mut code = ch as i16;
-        if code_a <= code && code <= code_z {
-            code = (code - code_a + shift + 26) % 26 + code_a;
-        }
-        result.push((code as u8) as char);
-    }
-    return result
+    let is_az = |c| (c >= 'A' && c <= 'Z');
+    let enc = |c: i16| (((c -code_a + shift + 26) % 26 + code_a) as u8) as char;
+    let cov = |c: char| if is_az(c) { enc(c as i16)} else { c };
+    return text.chars().map(|c| cov(c)).collect();
 }
-
-
 fn kakezan(num1: i64, num2: i64) -> i64 {
     return num1 * num2;
 }
