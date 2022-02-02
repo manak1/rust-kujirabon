4160d02757b00442ff825acadcf03843fc8a5386
manak1 <mana23happy@hotmail.co.jp>
2022-02-02 15:25:54 +0900

    feat: 標準体重を計算するプログラムを書いた

--- bmi/src/main.rs
+++ bmi/src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
+    let mut height;
+    loop {
+        println!("身長は?");
+        height = input_f(0.0);
+        if height > 0.0 { break; }
+        println!("正しい数値を入力してください")
+    }
+    let weight = 22.0 * (height/100.0).powf(2.0);
+    println!("標準体重は{:.1}", weight)
+}
+
+fn bmi() {
     let height_cm = input("身長(cm)は？");
     let weight = input("体重は(kg)は？");
     let height = height_cm / 100.0;
@@ -18,4 +30,18 @@ fn input(prompt: &str) -> f64 {
     let mut s  = String::new();
     std::io::stdin().read_line(&mut s).expect("入力エラー");
     return s.trim().parse().expect("数字変換エラー");
+}
+
+fn input_str()-> String {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).expect("入力エラー");
+    s.trim_end().to_string()
+}
+
+fn input_f(def: f64) -> f64 {
+    let s = input_str();
+    match s.trim().parse() {
+        Ok(v) => v,
+        Err(_)=> def,
+    }
 }
